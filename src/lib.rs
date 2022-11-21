a2227902eef5bfa5d0e4daaaac236836aa423b0a
bstrie <865233+bstrie@users.noreply.github.com>
2022-11-21 10:18:14 -0500

    test: import README into doc comment and remove cargo-readme
    Signed-off-by: bstrie <865233+bstrie@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-//! This crate implements a macro that defines ELF notes for communicating
-//! information to the tooling that processes ELF notes.
-//!
-//! Be sure to check out the [note section](https://docs.oracle.com/cd/E23824_01/html/819-0690/chapter6-18048.html)
-//! in the ELF specification.
-//!
-//! # Example
-//!
-//! ```
-//! use noted::noted;
-//! use goblin::Object;
-//!
-//! const YYYYY: &str = "yyyyy";
-//! const TWO: u32 = 2;
-//!
-//! noted! {
-//!     section = ".note.noted";
-//!
-//!     static FOO<"xxxxxxxx", 1, [u8; 4]> = [1, 2, 3, 4];
-//!     static BAR<YYYYY, TWO, u64> = 7;
-//!     static BAZ<"zzz", 3, u32> = 8;
-//! }
-//!
-//! fn main() {
-//!     // Load this binary
-//!     let path = std::env::current_exe().unwrap();
-//!     let buffer = std::fs::read(path).unwrap();
-//!     let elf = match Object::parse(&buffer).unwrap() {
-//!         Object::Elf(elf) => elf,
-//!         _ => panic!("unsupported type"),
-//!     };
-//!
-//!     // Parse and sort the notes in the specified section
-//!     let mut notes: Vec<_> = elf
-//!         .iter_note_sections(&buffer, Some(".note.noted"))
-//!         .unwrap()
-//!         .map(|x| x.unwrap())
-//!         .collect();
-//!     notes.sort_unstable_by_key(|x| x.n_type);
-//!
-//!     eprintln!("{:?}", notes);
-//!
-//!     // Validate all the notes
-//!     assert_eq!(3, notes.len());
-//!
-//!     assert_eq!(notes[0].n_type, 1);
-//!     assert_eq!(notes[1].n_type, TWO);
-//!     assert_eq!(notes[2].n_type, 3);
-//!
-//!     assert_eq!(notes[0].name, "xxxxxxxx");
-//!     assert_eq!(notes[1].name, YYYYY);
-//!     assert_eq!(notes[2].name, "zzz");
-//!
-//!     assert_eq!(notes[0].desc, &[1, 2, 3, 4]);
-//!     assert_eq!(notes[1].desc, &7u64.to_ne_bytes());
-//!     assert_eq!(notes[2].desc, &8u32.to_ne_bytes());
-//! }
-//! ```
-
+#![doc = include_str!("../README.md")]
 #![no_std]
 #![deny(clippy::all)]
 #![deny(missing_docs)]
